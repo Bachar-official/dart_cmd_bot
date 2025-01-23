6a1c16d8c3fe177ecb36b281780c515d93a23501
iBacharnikov <cjba4ar@gmail.com>
2025-01-23 11:33:20 +0500

    get config from env

--- bin/main.dart
+++ bin/main.dart
@@ -8,8 +8,8 @@ void main(List<String> arguments) async {
   if (Platform.isWindows) {
     ShowWindow(GetConsoleWindow(), SHOW_WINDOW_CMD.SW_HIDE);
   }
-  // String? token = Platform.environment['BOT_TOKEN'];
-  String? token = '1847335048:AAGfMb8lZh7y3R_h52qD4DJTkEmqke0kHmY';
+  String? token = Platform.environment['BOT_TOKEN'];
+  
 
   if (token == null) {
     throw Exception('Token is empty!');
