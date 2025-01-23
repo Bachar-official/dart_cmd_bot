fd8e33d37e2cbb9c38202d91a29c1e35848e32e8
iBacharnikov <cjba4ar@gmail.com>
2025-01-23 18:10:48 +0500

    Updated Linux install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 read -p "Please enter your Telegram bot token: " BOT_TOKEN
 
-LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/Bachar-official/dart_cmd_bot/releases/latest | grep "browser_download_url.*dart_cmd_bot_linux" | cut -d '"' -f 4)
-wget -O dart_cmd_bot_linux "$LATEST_RELEASE_URL"
-chmod +x dart_cmd_bot_linux
+LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/Bachar-official/dart_cmd_bot/releases/latest | grep "browser_download_url.*dart_cmd_bot_linux.exe" | cut -d '"' -f 4)
+wget -O dart_cmd_bot_linux.exe "$LATEST_RELEASE_URL"
+chmod +x dart_cmd_bot_linux.exe
 
 SERVICE_FILE="/etc/systemd/system/cmd_bot.service"
 cat <<EOF | sudo tee "$SERVICE_FILE" > /dev/null
@@ -13,7 +13,7 @@ Description=CMD Telegram Bot
 After=network.target
 
 [Service]
-ExecStart=$(pwd)/dart_cmd_bot_linux
+ExecStart=$(pwd)/dart_cmd_bot_linux.exe
 Restart=always
 Environment="BOT_TOKEN=$BOT_TOKEN"
 User=$USER
